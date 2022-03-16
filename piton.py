1b4ee8d0196ac0db7fbf2d68a8b2cc77942a4145
M.Salih <mseroglu47@hotmail.com>
2022-03-16 19:18:52 +0300

    piton dosyası eklendi

---
+++ piton.py
@@ -0,0 +1,8 @@
+
+aylar = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
+
+#for i in range(12):
+ #   print(aylar[i])
+
+for i in ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]:
+    print(i)
